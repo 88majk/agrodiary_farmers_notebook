6e6f94a2e945084d4b6cec8e9e4381c44533af11
88majk <mikolaj19gr@gmail.com>
2024-10-20 12:54:10 +0200

    Initial commit

---
+++ backend/Agrodiary/src/main/java/com/example/agrodiary/AgrodiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.agrodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrodiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrodiaryApplication.class, args);
+    }
+
+}
